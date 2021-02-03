f9b0e48a1d3af278b5efcd5da51414c6240b4f44
glepevedic <gillian.lepevedic@gmail.com>
2021-02-03 13:20:38 +0100

    Add GestionFichier.java

---
+++ GestionFichier.java
@@ -0,0 +1,21 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GestionFichier {
+    public static void main(String args[]) {   
+    try{
+      // Le fichier d'entrée
+      FileInputStream file = new FileInputStream("file.txt");   
+      Scanner scanner = new Scanner(file);  
+      
+      //renvoie true s'il y a une autre ligne à lire
+      while(scanner.hasNextLine()){
+        System.out.println(scanner.nextLine());
+      }
+      scanner.close();    
+    } catch (IOException e){
+        e.printStackTrace();
+    }
+  }
+}
